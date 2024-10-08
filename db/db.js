6836961e1ceefefb438d81aa58041dd6062f3237
Karthik7013 <karthiktumala143@gmail.com>
2024-10-08 15:14:14 +0530

    added env variables

--- db/db.js
+++ db/db.js
@@ -4,14 +4,14 @@ require('dotenv').config()
 async function connectToDatabase() {
     try {
         const connection = await mysql.createConnection({
-            host: process.env.HOST,
-            user: process.env.USER,
-            password: process.env.PASSWORD,
-            database: process.env.DATABASE
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
         });
         return connection;
     } catch (err) {
-        console.error('Error connecting to MySQL:', err);
+        console.error('Error connecting to MySQL:', err.message);
         process.exit(1); // Exit the process if there's a connection error
     }
 }
