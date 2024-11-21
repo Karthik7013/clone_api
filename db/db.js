72592f9be4f8b6f1acb699591fc8bda470f88dfc
Karthik7013 <karthiktumala143@gmail.com>
2024-11-22 02:17:29 +0530

    add urls

--- db/db.js
+++ db/db.js
@@ -4,22 +4,22 @@ require('dotenv').config()
 async function connectToDatabase() {
     try {
         // working with host db
-        // const connection = await mysql.createConnection({
-        //     host: process.env.DB_HOST,
-        //     user: process.env.DB_USER,
-        //     password: process.env.DB_PASSWORD,
-        //     database: process.env.DB_NAME,
-        //     port: process.env.DB_PORT,
-        // });
+        const connection = await mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            port: process.env.DB_PORT,
+        });
 
         // working with local db
-        const connection = await mysql.createConnection({
-            host: '127.0.0.1',
-            user: 'root',
-            password: 'root',
-            database: 'namelixdb',
-            port: 3306,
-        });
+        // const connection = await mysql.createConnection({
+        //     host: '127.0.0.1',
+        //     user: 'root',
+        //     password: 'root',
+        //     database: 'namelixdb',
+        //     port: 3306,
+        // });
         return connection;
     } catch (err) {
         console.error('Error connecting to MySQL:', err);
