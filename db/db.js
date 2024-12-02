c685899a38630ccd5681232e73a62481d789bb54
Karthik7013 <karthiktumala143@gmail.com>
2024-12-02 13:50:06 +0530

    rm logs

--- db/db.js
+++ db/db.js
@@ -2,13 +2,6 @@ const mysql = require('mysql2/promise');
 require('dotenv').config()
 
 async function connectToDatabase() {
-    console.log({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        port: process.env.DB_PORT,
-    },'db credentials')
     try {
         const connection = await mysql.createConnection({
             host: process.env.DB_HOST,
