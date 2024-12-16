dd0496cfc9c82e2d23c21cd86ce7ab40f28c558e
Karthik7013 <karthiktumala143@gmail.com>
2024-12-16 08:53:46 +0530

    added agent routes

--- route/v1/agent/agentRoutes.js
+++ route/v1/agent/agentRoutes.js
@@ -4,7 +4,12 @@ const { getAgentProfile } = require('../../../controller/agentController');
 
 
 const agentRoutes = Router();
-agentRoutes.get('/profile', isAuthenticated(['agent']), getAgentProfile)
+agentRoutes.get('/profile', isAuthenticated(['agent']), getAgentProfile);
+agentRoutes.post('/profile/update', isAuthenticated(['agent']), () => { });
+agentRoutes.post('/createPolicy', isAuthenticated(['agent']), () => { })
+agentRoutes.get('/policies', isAuthenticated(['agent']), () => { })
+agentRoutes.get('/claims', isAuthenticated(['agent']), () => { })
+agentRoutes.post('/register-claims', isAuthenticated(['agent']), () => { })
 
 
 module.exports = agentRoutes;
