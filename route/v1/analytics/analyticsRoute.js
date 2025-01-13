7524b50a9c5f5bbc685e3dfa4b3fed7abca85212
karthik7013 <karthiktumala143@gmail.com>
2025-01-14 00:29:51 +0530

    added return stmt

--- route/v1/analytics/analyticsRoute.js
+++ route/v1/analytics/analyticsRoute.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
 const analyticsRoutes = Router();
 
-analyticsRoutes.get('/cron-job', () => {
+analyticsRoutes.get('/cron-job', (req,res) => {
     console.log('cron-job triggered !')
+    return res.send('cron-job-triggered !');
 })
 
 module.exports = analyticsRoutes;
