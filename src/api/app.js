6490352c7ab270621ebe3a89674165b8b2747c42
karthik7013 <karthiktumala143@gmail.com>
2025-02-07 19:35:59 +0530

    Revert "serve static file"
    This reverts commit 487007d749e62888c41c5b9b56e2c4aece261b7f.

--- src/api/app.js
+++ src/api/app.js
@@ -35,8 +35,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(cookieParser());
-
-app.use('/public', express.static(path.join(__dirname, '../../public')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 
 app.use('/api/v1/auth', authRoutes)
