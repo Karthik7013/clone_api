5dbbf7746a50fd90775bdcdc75f0a8bcc805cdd6
Karthik7013 <karthiktumala143@gmail.com>
2025-02-08 10:49:48 +0530

    resolved err in error handler

--- src/api/middleware/errorHandler.js
+++ src/api/middleware/errorHandler.js
@@ -1,9 +1,12 @@
+const dotenv = require('dotenv')
+dotenv.config();
+const NODE_ENV = process.env.NODE_ENV || "DEVELOPMENT"
 const errorHandler = (err, req, res, next) => {
   res.status(err.status || 500).json({
     success: false,
     message: err.message || "Internal Server Error",
     error: {
-      stack: err.stack,
+      ...(NODE_ENV === "DEVELOPMENT" ? { stack: err.stack } : {}),
       details: err.details || "An unexpected error occurred."
     },
     status: err.status || 500,
