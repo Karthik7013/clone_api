aad5458a0dbf1a336d4c212776075d725945174a
karthik7013 <karthiktumala143@gmail.com>
2025-02-11 23:20:57 +0530

    added

--- src/api/v1/controller/authController.js
+++ src/api/v1/controller/authController.js
@@ -286,25 +286,25 @@ const verfiyEmployee = async (req, res, next) => {
         res.cookie('refreshToken', refreshToken, {
             httpOnly: true,
             maxAge: 3600000,
-            // secure: process.env.NODE_ENV === 'PRODUCTION'
             secure: process.env.NODE_ENV === 'PRODUCTION' && req.secure,
-            sameSite: process.env.NODE_ENV === 'PRODUCTION' ? 'Strict' : 'Lax'
+            // sameSite: process.env.NODE_ENV === 'PRODUCTION' ? 'Strict' : 'Lax'
+            sameSite: 'None'
         });
 
         res.cookie('accessToken', accessToken, {
             httpOnly: true,
             maxAge: 900000,
-            // secure: process.env.NODE_ENV === 'PRODUCTION'
             secure: process.env.NODE_ENV === 'PRODUCTION' && req.secure,
-            sameSite: process.env.NODE_ENV === 'PRODUCTION' ? 'Strict' : 'Lax'
+            // sameSite: process.env.NODE_ENV === 'PRODUCTION' ? 'Strict' : 'Lax',
+            sameSite: 'None'
         });
 
         res.cookie('role', 'employee', {
             httpOnly: true,
             maxAge: 3600000,
-            // secure: process.env.NODE_ENV === 'PRODUCTION'
             secure: process.env.NODE_ENV === 'PRODUCTION' && req.secure,
-            sameSite: process.env.NODE_ENV === 'PRODUCTION' ? 'Strict' : 'Lax'
+            // sameSite: process.env.NODE_ENV === 'PRODUCTION' ? 'Strict' : 'Lax',
+            sameSite: 'None'
         });
 
         return res.status(200).json(
