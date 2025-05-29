bdd3515a46774a5b2ef19268423771de1eb49453
karthik7013 <karthiktumala143@gmail.com>
2025-05-30 00:30:21 +0530

    rm unused pkgs

--- src/api/v1/routes/analyticsRoute.js
+++ src/api/v1/routes/analyticsRoute.js
@@ -1,45 +1,45 @@
 const { Router } = require('express');
 const analyticsRoutes = Router();
-const puppeteer = require('puppeteer');
+// const puppeteer = require('puppeteer');
 
 
 analyticsRoutes.get('/cron-job', async (req, res) => {
-        return res.status(200).json({});
-    const currentTime = new Date().toISOString();
-    // return res.send('cronjob-triggered !');
-    const browser = await puppeteer.launch({
-        headless: process.env.NODE_ENV === 'PRODUCTION',
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    });
-    const page = await browser.newPage();
-    await page.setViewport({ width: 1280, height: 800 });
+    return res.status(200).json({});
+    // const currentTime = new Date().toISOString();
+    // // return res.send('cronjob-triggered !');
+    // const browser = await puppeteer.launch({
+    //     headless: process.env.NODE_ENV === 'PRODUCTION',
+    //     args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    // });
+    // const page = await browser.newPage();
+    // await page.setViewport({ width: 1280, height: 800 });
 
-    // 1. Open login page
-    await page.goto('https://www.naukri.com/nlogin/login', { waitUntil: 'networkidle2' });
+    // // 1. Open login page
+    // await page.goto('https://www.naukri.com/nlogin/login', { waitUntil: 'networkidle2' });
 
-    // 2. Login
-    await page.type('#usernameField', process.env.NAUKRI_EMAIL, { delay: 50 });
-    await page.type('#passwordField', process.env.NAUKRI_PASSWORD, { delay: 50 });
+    // // 2. Login
+    // await page.type('#usernameField', process.env.NAUKRI_EMAIL, { delay: 50 });
+    // await page.type('#passwordField', process.env.NAUKRI_PASSWORD, { delay: 50 });
 
-    await Promise.all([
-        page.click('button[type="submit"]'),
-        page.waitForNavigation({ waitUntil: 'networkidle2' }),
-    ]);
-    // 3. Navigate to profile page
-    await page.goto('https://www.naukri.com/mnjuser/profile', { waitUntil: 'networkidle2' });
+    // await Promise.all([
+    //     page.click('button[type="submit"]'),
+    //     page.waitForNavigation({ waitUntil: 'networkidle2' }),
+    // ]);
+    // // 3. Navigate to profile page
+    // await page.goto('https://www.naukri.com/mnjuser/profile', { waitUntil: 'networkidle2' });
 
-    await page.click('.hdn> em');
-    // 4. Wait to ensure the page registers the refresh
+    // await page.click('.hdn> em');
+    // // 4. Wait to ensure the page registers the refresh
 
-    // await page.click('.uploadBtn');
-    await new Promise((resolve) => setTimeout(resolve, 5000));
-    await page.click('#saveBasicDetailsBtn');
-    await new Promise((resolve) => setTimeout(resolve, 5000));
-    await page.goto('https://www.naukri.com/mnjuser/profile', { waitUntil: 'networkidle2' });
-    await new Promise((resolve) => setTimeout(resolve, 5000));
-    console.log(`/============== NAUKRI PROFILE REFRESHED [${currentTime}]==================/`)
-    await browser.close();
-    return res.send('[cron-job] - Naukri Profile Updated');
+    // // await page.click('.uploadBtn');
+    // await new Promise((resolve) => setTimeout(resolve, 5000));
+    // await page.click('#saveBasicDetailsBtn');
+    // await new Promise((resolve) => setTimeout(resolve, 5000));
+    // await page.goto('https://www.naukri.com/mnjuser/profile', { waitUntil: 'networkidle2' });
+    // await new Promise((resolve) => setTimeout(resolve, 5000));
+    // console.log(`/============== NAUKRI PROFILE REFRESHED [${currentTime}]==================/`)
+    // await browser.close();
+    // return res.send('[cron-job] - Naukri Profile Updated');
 })
 
 module.exports = analyticsRoutes;
