8b9c71852beb4dea7a41c953371ea32a6fb7b664
Karthik7013 <karthiktumala143@gmail.com>
2025-06-17 14:10:07 +0530

    update query uploads added file_key col

--- src/api/config/queries.constants.js
+++ src/api/config/queries.constants.js
@@ -90,7 +90,7 @@ const DELETE_PERMISSION = 'DELETE FROM role_permissions WHERE permission_id = ? 
 
 const GET_EMPLOYEE_PERMISSIONS = 'SELECT employees.*,roles.department,roles.role_name, employee_roles.employee_role_id, COALESCE(JSON_ARRAYAGG(role_permissions.permission_id), JSON_ARRAY()) AS permissions FROM employees JOIN employee_roles ON employees.employee_id = employee_roles.employee_id JOIN roles on employee_roles.role_id = roles.role_id LEFT JOIN role_permissions ON employee_roles.employee_role_id = role_permissions.employee_role_id GROUP BY employees.employee_id, employee_roles.employee_role_id';
 
-const UPLOAD_FILE = `INSERT INTO media_files (file_name,file_url,file_type,mime_type,file_size) VALUES(?,?,?,?,?)`;
+const UPLOAD_FILE = `INSERT INTO media_files (file_name,file_key,file_url,file_type,mime_type,file_size) VALUES(?,?,?,?,?,?)`;
 
 // errors
 const INSERT_ERR_LOG = 'INSERT INTO ErrorLogs (ErrorMessage,ErrorType,Severity,ErrorSource,StackTrace,ErrorCode,UserID,IPAddress) VALUES(?,?,?,?,?,?,?,?)'
--- src/api/v1/handlers/uploadHandler.js
+++ src/api/v1/handlers/uploadHandler.js
@@ -8,6 +8,7 @@ const uploadHandler = async (formData, fileInfo) => {
             fieldname,
             originalname,
             mimetype,
+            filename,
             size
         } = fileInfo;
         const response = await axios.post('https://uploadimgur.com/upload', formData, {
@@ -17,7 +18,7 @@ const uploadHandler = async (formData, fileInfo) => {
         if (response.status !== 200 || !response.data || !response.data.link) {
             throw new Error('Failed to upload image: Invalid response from upload server');
         }
-        await connection.execute(UPLOAD_FILE, [originalname, response.data.link, fieldname, mimetype, size]);
+        await connection.execute(UPLOAD_FILE, [originalname, filename, response.data.link, fieldname, mimetype, size]);
         const fileUrl = response.data
         return fileUrl;
     } catch (error) {
