dad32dd460d2a6056a6c46e788353d3851bf7b84
Karthik7013 <karthiktumala143@gmail.com>
2025-07-15 12:34:47 +0530

    added logs for env

--- src/api/config/db.js
+++ src/api/config/db.js
@@ -16,13 +16,28 @@ async function connectToDatabase() {
     }
 }
 async function connectToSassProduct() {
-    console.log({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.SASS_CHATBOT_BUILDER,
-        port: process.env.DB_PORT,
-    });
+    console.log(
+        process.env.ACCESS_TOKEN_EXPIRES
+        , process.env.APP_PASSWORD
+        , process.env.CACHE_EXPIRE_TIME
+        , process.env.DB_HOST
+        , process.env.DB_NAME
+        , process.env.DB_PASSWORD
+        , process.env.DB_PORT
+        , process.env.DB_USER
+        , process.env.EMAIL
+        , process.env.GEMINI_API_KEY
+        , process.env.JWT_REFRESH_SECRET_KEY
+        , process.env.JWT_SECRET_KEY
+        , process.env.NODE_ENV
+        , process.env.PORT
+        , process.env.REDIS_SERVICE_URI
+        , process.env.REFRESH_TOKEN_EXPIRES
+        , process.env.USER_DATA_ENCRYPTION_KEY
+        , process.env.NAUKRI_EMAIL
+        , process.env.NAUKRI_PASSWORD
+        , process.env.SASS_CHATBOT_BUILDER
+    );
     try {
         const connection = await mysql.createConnection({
             host: process.env.DB_HOST,
